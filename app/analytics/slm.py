6eb9d1f47cc1f9ba7c7d8a9ebef1a7d26f6b1223
Tola Awofolu <oawofolu@pivotal.io>
2024-05-02 07:13:31 -0400

    Updated App

--- app/analytics/slm.py
+++ app/analytics/slm.py
@@ -12,8 +12,8 @@ import mlflow.pyfunc
 import logging
 import traceback
 import requests
-import onnxruntime_genai as og
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 load_dotenv()
 
@@ -50,31 +50,20 @@ def ingest_metadata_from_huggingface_model(model_name: str):
 
 
 def publish_slm(repo_name: str, pretrained_model_name: str):
-    # TODO: DO NOT HARDCODE!!!
-    clone_url = (f"https://tanzuml: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@huggingface.co/"
-                 f"tanzuml/{repo_name}")
-
     model_name = f"tanzuml/{repo_name}"
 
     print(f"=====================\nSaving model {model_name}...\n=====================\n")
 
-    model = AutoModelForCausalLM.from_pretrained(f'{pretrained_model_name}', trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(f'{pretrained_model_name}',
+                                                 torch_dtype=torch.bfloat16,
+                                                 device_map='cpu',
+                                                 low_cpu_mem_usage=True,
+                                                 trust_remote_code=True)
     tokenizer = AutoTokenizer.from_pretrained(f'{pretrained_model_name}')
-    model.save_pretrained(pretrained_model_name)
-    tokenizer.save_pretrained(pretrained_model_name)
 
-    os.system(f"git clone {clone_url}; "
-              f"cd {repo_name};"
-              "git config --global user.email 'tanzuml@example.com';"
-              "git config --global user.name 'Tanzu ML';"
-              f" git lfs install; "
-              f"huggingface-cli lfs-enable-largefiles .;"
-              f"mv ../{pretrained_model_name}/* .;"
-              f"rm -rf ../{pretrained_model_name}; "
-              "git add .;"
-              "git commit -m 'Uploaded pretrained model';"
-              f"git push; "
-              f"cd -; rm -rf {repo_name}")
+    # TODO: DO NOT HARDCODE!!!
+    model.push_to_hub(model_name, max_shard_size='2GB', use_auth_token='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')
+    tokenizer.push_to_hub(model_name, use_auth_token='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')
 
 
 def promote_model_to_staging(model_name, pipeline_name):
